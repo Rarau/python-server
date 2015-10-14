2d55fa1eaf28ced0bef23e05c303bd73d48dba04
Raul <rarau001@campus.goldsmiths.ac.uk>
2015-10-14 14:32:21 +0200

    Bind to any local ip

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 
 # code reduced from https://wiki.python.org/moin/BaseHttpServer
-
+import json
 import time
 import BaseHTTPServer
 
@@ -15,9 +15,31 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     s.wfile.write("<html><head><title>Title goes here.</title></head>")
     s.wfile.write("<body><p>This is a test.</p>")
     s.wfile.write("<p>You accessed path: %s</p>" % s.path)
-    s.wfile.write("</body></html>")
+    s.wfile.write("<ul>")
+
+    url_segments = s.path.split("/")
+	
+    for seg in url_segments:
+		s.wfile.write("<li>%s</li>" % seg)
+
+    data = url_segments[1].split("+")
+    result = 0
+    message = ""
+    for op in data:
+        result += int(op)
+        message += "+" + str(op) + " "
+        
+    message += " = " + str(result)
+    
+    
+    json_result = ["result", result]
+    json_result["operands"] = data
+
+    s.wfile.write("<p>%s</p>" % json.dumps(json_result))
+	
+    s.wfile.write("</ul></body></html>")
 
 
-httpd = BaseHTTPServer.HTTPServer(("localhost", 8000), MyHandler)
+httpd = BaseHTTPServer.HTTPServer(("0.0.0.0", 8000), MyHandler)
 httpd.serve_forever()
 
